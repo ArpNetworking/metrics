e51cd07ea96de297b3ce125b7ce5e1d9ddea0051
Brandon Arp <brandonarp@gmail.com>
2013-09-19 00:23:46 -0700

    sliding sidebar

--- app/assets/javascripts/app.ts
+++ app/assets/javascripts/app.ts
@@ -368,6 +368,8 @@ class AppViewModel
     connectionIndex: { [id:string] : ConnectionVM;} = {};
     viewDuration: ViewDuration = new ViewDuration();
     paused = ko.observable<boolean>(false);
+    metricsVisible = ko.observable<boolean>(true);
+    metricsWidth = ko.observable<boolean>(true);
     private colors: Color[] = [new Color(31,120,180), new Color(51,160,44), new Color(227,26,28), new Color(255,127,0),
         new Color(106,61,154), new Color(166,206,227), new Color(178,223,138), new Color(251,154,153),
         new Color(253,191,111), new Color(202,178,214), new Color(255,255,153)];
@@ -403,7 +405,16 @@ class AppViewModel
             this.connections.remove(cvm);
             delete this.connectionIndex[cvm.server]
         }
+        this.setMetricsWidth = () => {
+            this.metricsWidth(this.metricsVisible());
+        }
     }
+
+    toggleMetricsVisible() {
+        this.metricsVisible(!this.metricsVisible());
+    }
+    
+    setMetricsWidth: () => void;
 
     togglePause() {
         this.paused(!this.paused());
@@ -684,6 +695,7 @@ class AppViewModel
 interface KnockoutBindingHandlers {
     slider: KnockoutBindingHandler;
     legendBlock: KnockoutBindingHandler;
+    slide: KnockoutBindingHandler;
 }
 
 ko.bindingHandlers.slider = {
@@ -694,6 +706,27 @@ ko.bindingHandlers.slider = {
         $(element).dragslider(value);
     }
 };
+
+ko.bindingHandlers.slide = {
+    update: function(element, valueAccessor, allBindingsAccessor) {
+        var shouldShow = ko.utils.unwrapObservable(valueAccessor());
+        var bindings = allBindingsAccessor();
+        var direction = ko.utils.unwrapObservable(bindings.direction);
+        var duration = ko.utils.unwrapObservable(bindings.duration) || 400;
+        var after = ko.utils.unwrapObservable(bindings.after);
+
+        var effectOptions = { "direction": direction };
+        
+        if (shouldShow) {
+            after();
+            $(element).show("slide", effectOptions, duration);
+        } else {
+            $(element).hide("slide", effectOptions, duration, after);
+        }
+        
+        
+    }
+}
 
 ko.bindingHandlers.legendBlock = {
     init: function(element, valueAccessor) {
